b3d7b336711637cd20bf8deb9eac85e59de3d5a2
Vasiliy Bondarenko <vabondarenko@gmail.com>
2018-08-09 23:44:56 +0700

    Extract transactions while parsing raw block and save transactions on the block instance. Solves issue https://github.com/EOSIO/demux-js/issues/24

--- src/demux/readers/eos/NodeosBlock.ts
+++ src/demux/readers/eos/NodeosBlock.ts
@@ -1,29 +1,32 @@
 import { EosAction } from "./interfaces"
-import { Block } from "../../../../index"
+import {Block, Transaction} from "../../../../index"
 
 export class NodeosBlock implements Block {
   public actions: EosAction[]
+  public transactions: Transaction[]
   public blockHash: string
   public blockNumber: number
   public previousBlockHash: string
   constructor(rawBlock: any) {
-    this.actions = this.collectActionsFromBlock(rawBlock)
+    this.transactions = []
+    this.actions = []
     this.blockNumber = rawBlock.block_num
     this.blockHash = rawBlock.id
     this.previousBlockHash = rawBlock.previous
+    this.collectActionsAndTransactionsFromBlock(rawBlock)
   }
 
-  protected collectActionsFromBlock(rawBlock: any): EosAction[] {
-    return this.flattenArray(rawBlock.transactions.map((transaction: any) => {
+  protected collectActionsAndTransactionsFromBlock(rawBlock: any) {
+    rawBlock.transactions.forEach((transaction: any) => {
       if (!transaction.trx.transaction) {
-        return [] // Deferred transaction, cannot decode
+        return // Deferred transaction, cannot decode
       }
-      return transaction.trx.transaction.actions.map((action: any, actionIndex: number) => {
+      const actions = transaction.trx.transaction.actions.map((action: any, actionIndex: number) => {
         // Delete unneeded hex data if we have deserialized data
         if (action.data) {
           delete action.hex_data // eslint-disable-line
         }
-        return {
+        const formattedAction = {
           type: `${action.account}::${action.name}`,
           payload: {
             transactionId: transaction.trx.id,
@@ -31,12 +34,19 @@ export class NodeosBlock implements Block {
             ...action,
           },
         }
+        this.actions.push(formattedAction)
+        return formattedAction
       })
-    }))
-  }
-
-  private flattenArray(arr: any[]): any[] {
-    return arr.reduce((flat, toFlatten) =>
-      flat.concat(Array.isArray(toFlatten) ? this.flattenArray(toFlatten) : toFlatten), [])
+      // Delete unneeded data
+      delete transaction.trx.signatures
+      delete transaction.trx.packed_trx
+      // reattach same actions to transaction
+      transaction.trx.transaction.actions = actions
+      transaction.block_producer = rawBlock.producer
+      transaction.block_id = rawBlock.id
+      transaction.block_num = rawBlock.block_num
+      transaction.block_timestamp = rawBlock.timestamp
+      this.transactions.push(transaction)
+    })
   }
 }
